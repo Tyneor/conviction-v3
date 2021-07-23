7622d83c27f21950937af918704670fe93dd84e9
Tyneor <etienne.quarez@gmail.com>
2021-07-24 00:01:55 +0200

    cleaned arrow by creating calculate vec2 array : - every setter recalculate the polygon if the new value is different from the current one - circle radius and width can't go under 20 and 10 respectively - polygon shape calculation has been simplfied : default then small arrow then tiny arrow

--- scenes/Arrow.gd
+++ scenes/Arrow.gd
@@ -1,63 +1,57 @@
 extends Node2D
 
-var defaultVec2Array : Array
-var length = 0 setget set_length
-var width = 180 setget set_width
-var circle_radius = 100 setget set_circle_radius
-var vec2Array = []
+var length = 200 setget set_length
+var width = 70 setget set_width
+var circle_radius = 50 setget set_circle_radius
+var vec2_array = []
 
 func _ready():
-	self.defaultVec2Array = [
-		Vector2(width / 2, 0), 
-		Vector2(width / 2, 0), 
-		Vector2(width, 0), 
-		Vector2(0, 0), 
-		Vector2(- width, 0), 
-		Vector2(- width / 2, 0),
-		Vector2(- width / 2, 0) 
-	]
-	
+	self._calculate_vec2_array()
+
 func set_circle_radius(new_circle_radius):
-	circle_radius = max(new_circle_radius, 10)
-	if self.circle_radius < self.width / 2:
-		self.width = self.circle_radius * 2
-	self.update()
+	if max(new_circle_radius, 20) != self.circle_radius:
+		circle_radius = max(new_circle_radius, 10)
+		if self.circle_radius < self.width / 2:
+			self.width = self.circle_radius * 2
+		self._calculate_vec2_array()
 
 func set_width(new_width: int):
-	width = max(new_width, 20)
-	if self.width > self.circle_radius * 2:
-		print(self.width, " ", self.circle_radius)
-		self.circle_radius = self.width / 2
-	self.defaultVec2Array = [
-		Vector2(width / 2, 0), 
-		Vector2(width / 2, 0), 
-		Vector2(width, 0), 
-		Vector2(0, 0), 
-		Vector2(- width, 0), 
-		Vector2(- width / 2, 0),
-		Vector2(- width / 2, 0) 
-	]
-	self.update()
+	if max(new_width, 20) != self.width:
+		width = max(new_width, 20)
+		if self.width > self.circle_radius * 2:
+			print(self.width, " ", self.circle_radius)
+			self.circle_radius = self.width / 2
+		self._calculate_vec2_array()
 
 func set_length(new_length: int):
-	length = new_length
-	self.vec2Array = PoolVector2Array(defaultVec2Array)
-	for i in range(1, 6):
-		self.vec2Array.set(i, self.vec2Array[i] + Vector2(0, length - sign(length) * width))
-	if length != 0:
-		self.vec2Array.set(3, Vector2(0, length))
-	if abs(length) <= width * 1.5 and abs(length) >= width:
-		self.vec2Array.set(2, self.vec2Array[2] + Vector2(abs(length) - 1.5 * width, 0))
-		self.vec2Array.set(4, self.vec2Array[4] + Vector2(-abs(length) + 1.5 * width, 0))
-	if abs(length) < width:
-		self.vec2Array.set(1, Vector2(0, 0))
-		self.vec2Array.set(2, Vector2(0.5 * width, 0))
-		self.vec2Array.set(4, Vector2(- 0.5 * width, 0))
-		self.vec2Array.set(5, Vector2(0, 0))
+	if new_length != self.length:
+		length = new_length
+		self._calculate_vec2_array()
+	
+func _calculate_vec2_array():
+	self.vec2_array = PoolVector2Array([
+		Vector2(width / 2, 0), 
+		Vector2(width / 2, length - sign(length) * width), 
+		Vector2(width, length - sign(length) * width), 
+		Vector2(0, length), 
+		Vector2(- width, length - sign(length) * width), 
+		Vector2(- width / 2, length - sign(length) * width),
+		Vector2(- width / 2, 0) 
+	])
+	if length == 0:
+		self.vec2_array.set(3, Vector2(0, 0))
+	if abs(length) <= width * 1.5:
+		self.vec2_array.set(2, Vector2(max(0.5 * width, abs(length) - 0.5 * width), self.vec2_array[2].y))
+		self.vec2_array.set(4, Vector2(- max(0.5 * width, abs(length) - 0.5 * width), self.vec2_array[4].y))
+	if abs(length) <= width:
+		self.vec2_array.remove(5)
+		self.vec2_array.remove(4)
+		self.vec2_array.remove(2)
+		self.vec2_array.remove(1)
 	self.update()
 
 func _draw():
-	draw_polygon(self.vec2Array, PoolColorArray([Color(1, 1, 1)]), [])
+	draw_polygon(self.vec2_array, PoolColorArray(), [])
 	draw_circle(Vector2(0, 0), self.circle_radius, Color(1, 1, 1))
 
 func _input(event):
